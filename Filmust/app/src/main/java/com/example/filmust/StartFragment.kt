0b74d3b4268c96d935d9a9ba6119bc8427657300
MAnikaev <anikaev.maks@gmail.com>
2023-07-06 14:06:44 +0300

    Добавлен Splash Screen

--- Filmust/app/src/main/java/com/example/filmust/StartFragment.kt
+++ Filmust/app/src/main/java/com/example/filmust/StartFragment.kt
@@ -1,5 +1,6 @@
 package com.example.filmust
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.filmust.databinding.FragmentStartBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class StartFragment : Fragment(R.layout.fragment_start) {
    var binding : FragmentStartBinding? = null
@@ -14,7 +19,12 @@ class StartFragment : Fragment(R.layout.fragment_start) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentStartBinding.bind(view)
         HttpResponseGetter.getMoviesListByUrl("https://moviesdatabase.p.rapidapi.com/titles/x/upcoming", TypeOfMovieList.Upcoming)
-        binding?.btn?.setOnClickListener{
+
+        CoroutineScope(Dispatchers.Main).launch {
+            binding?.progressBar?.max = 10000
+            val value = 9900
+            ObjectAnimator.ofInt(binding?.progressBar, "progress", value).setDuration(2000).start()
+            delay(2000L)
             findNavController().navigate(R.id.action_startFragment_to_searchFragment)
         }
     }
