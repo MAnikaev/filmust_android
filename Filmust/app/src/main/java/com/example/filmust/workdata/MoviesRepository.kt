e05e6c3fff80297dec497d0410984a6fb5b19dce
asflp <as.flp@mail.ru>
2023-07-10 13:28:31 +0300

    add firebase manager

--- Filmust/app/src/main/java/com/example/filmust/workdata/MoviesRepository.kt
+++ Filmust/app/src/main/java/com/example/filmust/workdata/MoviesRepository.kt
@@ -3,4 +3,6 @@ package com.example.filmust.workdata
 object MoviesRepository {
     var upcomingMovies : List<Movie> = mutableListOf()
     var searchedMovies : List<Movie> = mutableListOf()
+    var favoriteMovies : MutableList<Movie>? = mutableListOf()
+    var viewedMovies : MutableList<Movie>? = mutableListOf()
 }
