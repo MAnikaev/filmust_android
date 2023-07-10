c4c6e4589d2d7d828d950e0de8d59f40fe77a1c9
Guzel Khazratova <guzkhaz04@gmail.com>
2023-07-10 20:18:43 +0300

    DetailFragment

--- Filmust/app/src/main/java/com/example/filmust/fragments/DetailFragment.kt
+++ Filmust/app/src/main/java/com/example/filmust/fragments/DetailFragment.kt
@@ -1,88 +1,101 @@
 package com.example.filmust.fragments
 
+import android.R
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.example.filmust.R
 import com.example.filmust.databinding.FragmentDetailBinding
+import com.example.filmust.workdata.HttpResponseGetter
 import com.example.filmust.workdata.Movie
+import com.example.filmust.workdata.MoviesRepository
 
 
-class DetailFragment : Fragment(R.layout.fragment_detail) {
+class DetailFragment : Fragment(com.example.filmust.R.layout.fragment_detail) {
 
-    private var binding: FragmentDetailBinding? = null
+    private lateinit var binding: FragmentDetailBinding
 
-    var imageView: ImageView? = null
+    private fun findById(id: String?) : Movie? {
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
-
-        super.onViewCreated(view, savedInstanceState)
-
-        binding = FragmentDetailBinding.bind(view)
-
-        imageView = getView()?.findViewById(R.id.imageView)
-
-        var movie = arguments?.get("ARG_ID")
-
-        if (movie != null){
-
-            movie = movie as Movie
-
-            Glide.with(this).load(movie.primaryImage?.url).into(binding!!.imageView)
-
-            binding?.run {
-                tvTitle?.text = movie.titleText.text
-
-                tvDesc?.text = "The film ${movie.titleText.text}, shot in the genres: ${movie.genres!!.genres[0]}" +
-                        "and ${movie.genres!!.genres[1]}" +
-                        "and lasting ${movie.runtime!!.seconds} seconds, was released in ${movie.releaseDate.day}" +
-                        "${movie.releaseDate.month} ${movie.releaseDate.year}. At the moment, " +
-                        "his rating is ${movie.meterRanking!!.currentRank}. Description: ${movie.plot?.plotText}"
-            }
+        for (movie in MoviesRepository.searchedMovies) {
+            if(movie.id == id)
+                return movie
         }
 
-        binding?.run {
-            historyBtn.setOnClickListener(){
+        for (movie in MoviesRepository.upcomingMovies) {
+            if(movie.id == id)
+                return movie
+        }
+
+        for (movie in MoviesRepository.favoriteMovies!!) {
+            if(movie.id == id)
+                return movie
+        }
+
+        for (movie in MoviesRepository.viewedMovies!!) {
+            if(movie.id == id)
+                return movie
+        }
+        return null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        super.onCreate(savedInstanceState)
+
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        var movieId = arguments?.getString("MOVIE_ID")
+
+        val movie = findById(movieId)
+
+        Glide.with(this).load(movie?.primaryImage?.url).into(binding.imageView)
+
+        binding.tvTitle.text = "${movie?.titleText?.text}"
+
+        binding.tvDesc.text = "The film ${movie?.titleText?.text} was released in ${movie?.releaseDate?.day}." +
+                "${movie?.releaseDate?.month} ${movie?.releaseDate?.year}"
+
+        binding.run {
+            historyBtn.setOnClickListener() {
                 //Сначала идет проверка на наличие в списке и затем смена на нужный цвет
                 //TODO: if (id нет в списке)
                 //{
-                historyBtn.setBackgroundColor(getResources().getColor(R.color.historyButtonColor_on))
+                historyBtn.setBackgroundColor(getResources().getColor(R.color.white))
                 //TODO: добавление в список
                 //}
                 //TODO: else(id есть в списке)
                 //{
-                historyBtn.setBackgroundColor(getResources().getColor(R.color.buttonColor_off))
+                historyBtn.setBackgroundColor(getResources().getColor(R.color.black))
                 //TODO: удаление из списка
                 //}
             }
         }
 
 
-        binding?.run {
-            likeBtn.setOnClickListener(){
+        binding.run {
+            likeBtn.setOnClickListener() {
 
                 //Сначала идет проверка на наличие в списке и затем смена на нужный цвет
                 //TODO: if (id нет в списке)
                 //{
-                likeBtn.setBackgroundColor(getResources().getColor(R.color.favButtonColor_on))
+                likeBtn.setBackgroundColor(getResources().getColor(R.color.white))
                 //TODO: добавление в список
                 //}
                 //TODO: else(id есть в списке)
                 //{
-                likeBtn.setBackgroundColor(getResources().getColor(R.color.buttonColor_off))
+                likeBtn.setBackgroundColor(getResources().getColor(R.color.black))
                 //TODO: удаление из списка
                 //}
             }
-
         }
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
+        return binding.root
     }
-
 }
